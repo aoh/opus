2ce6943d2525469881d5c7751f8f46baa0d9a3f7
Aki Helin <aki.helin@iki.fi>
2016-09-30 21:57:30 +0300

    added some tag search tests and killing previous opus instances after failed tests

--- test/usage.py
+++ test/usage.py
@@ -7,6 +7,9 @@ browser.get('http://localhost:9000')
 
 assert 'opus' in browser.title
 
+def press(text): 
+   browser.find_element_by_link_text(text).click()
+
 def fail_login(user, password):
    browser.get('http://localhost:9000/login')
    browser.find_element_by_name('login').send_keys(user)
@@ -36,6 +39,15 @@ def add_new_blag(id, content):
 def see(str):
    assert str in browser.page_source
    
+def nosee(str):
+   assert str not in browser.page_source
+   
+def seetag(str):
+   assert ("\">" + str + "\n") in browser.page_source
+   
+def noseetag(str):
+   assert ("\">" + str + "\n") not in browser.page_source
+   
 def search(str):
    browser.find_element_by_name('q').send_keys(str + Keys.RETURN)
 
@@ -60,6 +72,72 @@ add_new_blag("foobar", "# AB\n#foo #bar")
 add_new_blag("foo", "# A\n#foo")
 add_new_blag("bar", "# B\n#bar")
 add_new_blag("baz", "# C\n#baz")
+
+press("idx")
+seetag("ABC")
+seetag("AB")
+seetag("BC")
+seetag("AC")
+seetag("A")
+seetag("B")
+seetag("C")
+nosee("Selected")
+
+press("foo")
+seetag("ABC")
+seetag("AB")
+noseetag("BC")
+seetag("AC")
+seetag("A")
+noseetag("B")
+noseetag("C")
+see("Selected")
+
+press("bar")
+seetag("ABC")
+seetag("AB")
+noseetag("BC")
+noseetag("AC")
+noseetag("A")
+noseetag("B")
+noseetag("C")
+
+press("baz")
+seetag("ABC")
+noseetag("AB")
+noseetag("BC")
+noseetag("AC")
+noseetag("A")
+noseetag("B")
+noseetag("C")
+
+press("foo")
+seetag("ABC")
+noseetag("AB")
+seetag("BC")
+noseetag("AC")
+noseetag("A")
+noseetag("B")
+noseetag("C")
+
+press("bar")
+seetag("ABC")
+noseetag("AB")
+seetag("BC")
+seetag("AC")
+noseetag("A")
+noseetag("B")
+seetag("C")
+
+press("baz")
+seetag("ABC")
+seetag("AB")
+seetag("BC")
+seetag("AC")
+seetag("A")
+seetag("B")
+seetag("C")
+nosee("Selected")
 
 browser.quit()
 
